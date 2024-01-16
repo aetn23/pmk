9dd829f2560ebc89bdd4649a28a371f97217459c
aetn23 <mikolajpirog@gmail.com>
2024-01-16 13:08:12 +0100

    a

--- projekt/leds_main.c
+++ projekt/leds_main.c
@@ -16,35 +16,13 @@
 #define ACCEL_OUT_Y 0x2B
 #define ACCEL_OUT_Z 0x2D
 
-#define RED_LED_GPIO GPIOA
-#define GREEN_LED_GPIO GPIOA
-#define BLUE_LED_GPIO GPIOB
-#define GREEN2_LED_GPIO GPIOA
-#define RED_LED_PIN 6
-#define GREEN_LED_PIN 7
-#define BLUE_LED_PIN 0
-#define GREEN2_LED_PIN 5
-
-#define GREEN_POS 0
-#define RED_POS 1
-#define BLUE_POS 2
-#define GREEN2_POS 3
-
-#define RedLEDon() RED_LED_GPIO->BSRR = 1 << (RED_LED_PIN + 16)
-#define RedLEDoff() RED_LED_GPIO->BSRR = 1 << RED_LED_PIN
-
-#define BlueLEDon() BLUE_LED_GPIO->BSRR = 1 << (BLUE_LED_PIN + 16)
-#define BlueLEDoff() BLUE_LED_GPIO->BSRR = 1 << BLUE_LED_PIN
-
-#define GreenLEDon() GREEN_LED_GPIO->BSRR = 1 << (GREEN_LED_PIN + 16)
-#define GreenLEDoff() GREEN_LED_GPIO->BSRR = 1 << GREEN_LED_PIN
-
-#define Green2LEDon() GREEN2_LED_GPIO->BSRR = 1 << GREEN2_LED_PIN
-#define Green2LEDoff() GREEN2_LED_GPIO->BSRR = 1 << (GREEN2_LED_PIN + 16)
-#define TIMEOUT 100000000
-
 #define ITBUFEN (1 << 10)
 #define ITEVTEN (1 << 9)
+
+#define I2C_SPEED_HZ 100000
+#define PCLK1_MHZ 16
+
+#define BUF_MAX_SIZE 255 
 
 void usart_out(char *str) {
   for (size_t i = 0; str[i] != '\0';) {
@@ -53,6 +31,94 @@ void usart_out(char *str) {
       i++;
     }
   }
+}
+void configure_rcc() {
+  RCC->AHB1ENR |= RCC_AHB1ENR_GPIOBEN | RCC_AHB1ENR_DMA1EN;
+  RCC->AHB1ENR |= RCC_AHB1ENR_GPIOAEN;
+  RCC->AHB1ENR |= RCC_AHB1ENR_GPIOCEN;
+  RCC->APB1ENR |= RCC_APB1ENR_USART2EN;
+  RCC->APB2ENR |= RCC_APB2ENR_SYSCFGEN;
+  __NOP();
+}
+
+void configure_usart() {
+  GPIOafConfigure(GPIOA, 2, GPIO_OType_PP, GPIO_Fast_Speed, GPIO_PuPd_NOPULL,
+                  GPIO_AF_USART2);
+  GPIOafConfigure(GPIOA, 3, GPIO_OType_PP, GPIO_Fast_Speed, GPIO_PuPd_UP,
+                  GPIO_AF_USART2);
+  USART2->CR1 = USART_CR1_RE | USART_CR1_TE;
+  USART2->CR2 = 0;
+  USART2->BRR = (PCLK1_HZ + (BAUD / 2U)) / BAUD;
+  USART2->CR3 = USART_CR3_DMAT;
+  USART2->CR1 |= USART_Enable;
+}
+
+void configure_dma() {
+  DMA1_Stream6->CR =
+      4U << 25 | DMA_SxCR_PL_1 | DMA_SxCR_MINC | DMA_SxCR_DIR_0 | DMA_SxCR_TCIE;
+  DMA1_Stream6->PAR = (uint32_t)&USART2->DR;
+
+  DMA1->HIFCR = DMA_HIFCR_CTCIF6;
+  NVIC_EnableIRQ(DMA1_Stream6_IRQn);
+}
+
+static char dma_to_send[BUF_MAX_SIZE];
+static char buffer[BUF_MAX_SIZE];
+static uint8_t dma_size = 0;
+static uint8_t buf_size = 0;
+char array_use = 0;
+
+void append_to_comms(const char *str) {
+  size_t i = 0;
+  if (array_use) {
+    while (dma_size < BUF_MAX_SIZE && str[i] != 0) {
+      dma_to_send[dma_size++] = str[i++];
+      // usart_out(buffer);
+    }
+    dma_to_send[dma_size++] = 0;
+
+  } else {
+    while (buf_size < BUF_MAX_SIZE && str[i] != 0) {
+      buffer[buf_size++] = str[i++];
+      // usart_out(buffer);
+    }
+    buffer[buf_size++] = 0;
+  }
+  // usart_out(buffer);
+  //   usart_out(str);
+}
+
+void dma_send() {
+  irq_level_t level = IRQprotectAll();
+  uint8_t size = array_use ? dma_size : buf_size;
+  if (size > 0 && (DMA1_Stream6->CR & DMA_SxCR_EN) == 0 &&
+      (DMA1->HISR & DMA_HISR_TCIF6) == 0) {
+    // usart_out("send\n");
+    //     usart_out(buffer);
+    if (array_use) {
+      DMA1_Stream6->M0AR = (uint32_t)dma_to_send;
+      DMA1_Stream6->NDTR = dma_size;
+    } else {
+      DMA1_Stream6->M0AR = (uint32_t)buffer;
+      DMA1_Stream6->NDTR = buf_size;
+    }
+    array_use = ~array_use;
+    DMA1_Stream6->CR |= DMA_SxCR_EN;
+    dma_size = 0;
+    buf_size = 0;
+  }
+  IRQunprotectAll(level);
+}
+
+void DMA1_Stream6_IRQHandler() {
+  //  usart_out("dma inter hit\n");
+  irq_level_t level = IRQprotectAll();
+  uint32_t isr = DMA1->HISR;
+  if (isr & DMA_HISR_TCIF6) {
+    DMA1->HIFCR = DMA_HIFCR_CTCIF6;
+    dma_send();
+  }
+  IRQunprotectAll(level);
 }
 
 void int8_to_bits(char *buffer, int8_t val, char id) {
@@ -66,18 +132,18 @@ void int8_to_bits(char *buffer, int8_t val, char id) {
   buffer[10] = '\0';
 }
 
-typedef struct rec_data {
+typedef struct i2c_transer_data {
   char reg;
   char slave_addr;
   char to_send;
-} rec_data;
+} i2c_transfer_data;
 
-rec_data data;
+i2c_transfer_data data;
 
-char state = 0;
+uint8_t state = 0;
 
 void I2C1_EV_IRQHandler() {
-  irq_level_t level = IRQprotectAll();
+  //  irq_level_t level = IRQprotectAll();
   // //usart_out("Begin inter\n");
   if (state & (1 << 7)) {
     if (!(state & (1 << 0)) && I2C1->SR1 & I2C_SR1_SB) {
@@ -108,10 +174,7 @@ void I2C1_EV_IRQHandler() {
     }
 
   } else if (state & (1 << 6)) {
-    // TIM3->CR1 &= ~TIM_CR1_CEN; // turn of timer so it won't intefere
-    // TIM3->DIER &= ~TIM_DIER_UIE;
     I2C1->CR2 |= ITBUFEN;
-    // TIM3->SR |= (TIM_SR_UIF);
     if (!(state & (1 << 0)) && I2C1->SR1 & I2C_SR1_SB) {
       // usart_out("sb_send hit\n");
       I2C1->DR = data.slave_addr << 1;
@@ -151,34 +214,26 @@ void I2C1_EV_IRQHandler() {
       char reg = 'x';
       if (data.reg == ACCEL_OUT_Y)
         reg = 'y';
-      char buf[20];
+      char buf[11];
       int8_to_bits(buf, res, reg);
-      usart_out(buf);
+      append_to_comms(buf);
+      dma_send();
+      // usart_out(buf);
       state = 0;
       state |= (1 << 6);
       if (data.reg == ACCEL_OUT_X)
         data.reg = ACCEL_OUT_Y;
       else {
-        //        I2C1->CR1 |= I2C_CR1_START;
         data.reg = ACCEL_OUT_X;
       }
-      //      TIM3->CR1 |= TIM_CR1_CEN;
-      //      TIM3->DIER = TIM_DIER_UIE;
     }
   }
 
   // usart_out("end inter\n");
-  IRQunprotectAll(level);
+  //  IRQunprotectAll(level);
 }
 
-void I2C1_ER_IRQHandler() { usart_out("ER hit\n"); }
-
-uint8_t my_abs(int16_t x) {
-  return x;
-  return x = x & 0x80 ? ~((~x + 1) & 0xFF) : x;
-}
-
-void config_i2c() {
+void configure_i2c() {
   RCC->AHB1ENR |= RCC_AHB1ENR_GPIOBEN;
   RCC->APB1ENR |= RCC_APB1ENR_I2C1EN;
   GPIOafConfigure(GPIOB, 8, GPIO_OType_OD, GPIO_Low_Speed, GPIO_PuPd_NOPULL,
@@ -186,8 +241,6 @@ void config_i2c() {
   GPIOafConfigure(GPIOB, 9, GPIO_OType_OD, GPIO_Low_Speed, GPIO_PuPd_NOPULL,
                   GPIO_AF_I2C1);
   I2C1->CR1 = 0;
-#define I2C_SPEED_HZ 100000
-#define PCLK1_MHZ 16
   I2C1->CCR = (PCLK1_MHZ * 1000000) / (I2C_SPEED_HZ << 1);
   I2C1->CR2 = PCLK1_MHZ;
   I2C1->TRISE = PCLK1_MHZ + 1;
@@ -195,35 +248,13 @@ void config_i2c() {
   I2C1->CR2 |= ITBUFEN;
   I2C1->CR2 |= ITEVTEN;
   NVIC_EnableIRQ(I2C1_EV_IRQn);
-  //  NVIC_EnableIRQ(I2C1_ER_IRQn); <-- error interupt, what for?
 }
 
-void configure_rcc() {
-  RCC->AHB1ENR |= RCC_AHB1ENR_GPIOBEN;
-  RCC->AHB1ENR |= RCC_AHB1ENR_GPIOAEN;
-  RCC->AHB1ENR |= RCC_AHB1ENR_GPIOCEN;
-  RCC->APB1ENR |= RCC_APB1ENR_USART2EN;
-  __NOP();
-}
-
-void configure_usart() {
-  GPIOafConfigure(GPIOA, 2, GPIO_OType_PP, GPIO_Fast_Speed, GPIO_PuPd_NOPULL,
-                  GPIO_AF_USART2);
-
-  GPIOafConfigure(GPIOA, 3, GPIO_OType_PP, GPIO_Fast_Speed, GPIO_PuPd_UP,
-                  GPIO_AF_USART2);
-
-  USART2->CR1 = USART_Mode_Rx_Tx | USART_WordLength_8b | USART_Parity_No;
-  USART2->CR2 = USART_StopBits_1;
-  USART2->CR3 = USART_FlowControl_None;
-  USART2->BRR = (PCLK1_HZ + (BAUD / 2U)) / BAUD;
-  USART2->CR1 |= USART_Enable;
-}
 void configure_timer() {
   RCC->APB1ENR |= RCC_APB1ENR_TIM3EN;
   TIM3->CR1 = 0; // counting up
   TIM3->ARR = 16000;
-  TIM3->PSC = 10; // update 10 times per second
+  TIM3->PSC = 10; // update 100 times per second
   TIM3->EGR = TIM_EGR_UG;
   TIM3->SR = ~(TIM_SR_UIF);
   TIM3->DIER = TIM_DIER_UIE;
@@ -232,33 +263,27 @@ void configure_timer() {
 
 void TIM3_IRQHandler(void) {
   irq_level_t l = IRQprotectAll();
-  //usart_out("t\n");
+  // usart_out("t\n");
   uint32_t it_status = TIM3->SR & TIM3->DIER;
   if (it_status & TIM_SR_UIF) {
     TIM3->SR = ~TIM_SR_UIF;
     I2C1->CR1 |= I2C_CR1_START;
   }
 
- // if (it_status & TIM_SR_CC1IF) {
- //   TIM3->SR = ~TIM_SR_CC1IF;
- // }
+  // if (it_status & TIM_SR_CC1IF) {
+  //   TIM3->SR = ~TIM_SR_CC1IF;
+  // }
 
   IRQunprotectAll(l);
 }
 
 int main() {
-  config_i2c();
+  configure_i2c();
   configure_rcc();
   configure_usart();
   configure_timer();
-  // I2C1->CR2 &= ~ITBUFEN;
-  // I2C1->CR2 &= ~ITEVTEN;
+  configure_dma();
   char test = 0x47;
-  // write_i2c(ACCEL_CTRL_REG1, &test, 1, LIS35DE_ADDR);
-  // int8_t x = my_abs(read_i2c(ACCEL_OUT_X, LIS35DE_ADDR));
-  // int8_to_bits(debug_buffer, x, 'x');
-  // I2C1->CR2 |= ITBUFEN;
-  // I2C1->CR2 |= ITEVTEN;
   state |= (1 << 7);
   data.to_send = test;
   data.slave_addr = LIS35DE_ADDR;
@@ -267,31 +292,6 @@ int main() {
   Delay(1000000);
   TIM3->CR1 |= TIM_CR1_CEN;
   for (;;) {
-    // data.sending = 0;
-    //    state |= (1 << 6);
-    // I2C1->CR1 |= I2C_CR1_START;
-
-    // Delay(1000000);
-    //  data.reg = ACCEL_OUT_X;
-    //  data.slave_addr = LIS35DE_ADDR;
-    //  I2C1->CR1 |= I2C_CR1_START;
-    //  Delay(100000000);
-
-    // return 0;
-    //  int8_t x = my_abs(read_i2c(ACCEL_OUT_X, LIS35DE_ADDR));
-
-    // int8_t y = my_abs(read_i2c(ACCEL_OUT_Y, LIS35DE_ADDR));
-    // int8_t z = my_abs(read_i2c(ACCEL_OUT_Z, LIS35DE_ADDR));
-    // char outout[] = {'x', x, 'y', y, 'z', z, '\n', '\0'};
-    // char newlin[] = {'\n', 0};
-    // Delay(10000000);
-    ////    ////usart_out(outout);
-    //    int8_to_bits(debug_buffer, x, 'x');
-    //    ////usart_out(debug_buffer);
-    //// int8_to_bits(debug_buffer, z, 'z');
-    //// ////usart_out(debug_buffer);
-    //// int8_to_bits(debug_buffer, y, 'y');
-    //// ////usart_out(debu
   }
   return 0;
 }
--- projekt/move_mouse.py
+++ projekt/move_mouse.py
@@ -4,21 +4,22 @@ import mouse
 def main():
     stm32_dev = open("/dev/ttyACM0", "r")
     for line in stm32_dev:
-        if (line[0] == '0' or line[0] == '1') and line.__len__ == :
+        if (line[0] == '0' or line[0] == '1') and (line.__len__() == 10 ):
             val = int(line[0:8], 2)
             bytes = val.to_bytes(1, byteorder=sys.byteorder, signed=False)
             final_val = int.from_bytes(bytes, byteorder=sys.byteorder, signed=True);
             print(line[8] + ': ' + str(final_val))
             x_val = 0 
             y_val = 0
-            if final_val > 0 and final_val < 8:
+            if (final_val > 0 and final_val < 9) or (final_val < 0 and final_val >-9):
                 final_val = 0
+
 #            final_val *= 2
             if line[8] == 'x':
                 x_val = final_val
             else:
                 y_val = final_val
-            mouse.move(x_val, y_val, absolute=False, duration=0);
+            mouse.move(y_val, x_val, absolute=False, duration=0);
 
 if __name__ == "__main__":
     main()
